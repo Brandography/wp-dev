f276776d4e5b7dfc7a049bdbd21f37bb40bb66d0
Daniel Horton-Wellings <danielhortonwellings@gmail.com>
2018-06-05 16:35:58 -0500

    Initial commit

---
+++ db_reset.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+if [ -z "$1" ]
+  then
+    echo "enter sql file name"
+else
+    docker-compose exec -T -u root db mysql -uwordpress -pwordpress wordpress < $@
+    docker-compose exec -T -u root db mysql -uwordpress -pwordpress wordpress --execute="update wp_options set option_value='http://localhost/' where option_name='siteurl';"
+    docker-compose exec -T -u root db mysql -uwordpress -pwordpress wordpress --execute="update wp_options set option_value='http://localhost/' where option_name='home';"
+fi
---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+docker-compose up -d
+echo "
+===================== 🚀 Done 🚀 ===================
+
+      Access your new links:
+
+      🌎 Web server: http://localhost/
+      ⚙️ PHPMyAdmin: http://localhost:8080
+
+===================== 🚀 Done 🚀 ==================="
