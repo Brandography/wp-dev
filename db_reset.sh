a544f17975e744ae53225953d9666018516ae3b2
Daniel Horton-Wellings <danielhortonwellings@gmail.com>
2018-06-06 10:49:25 -0500

    Updated db reset script to import gz compressed files + added db dump script

--- db_reset.sh
+++ db_reset.sh
@@ -3,7 +3,7 @@ if [ -z "$1" ]
   then
     echo "enter sql file name"
 else
-    docker-compose exec -T -u root db mysql -uwordpress -pwordpress wordpress < $@
+    zcat $@ | docker-compose exec -T -u root db mysql -uwordpress -pwordpress wordpress
     docker-compose exec -T -u root db mysql -uwordpress -pwordpress wordpress --execute="update wp_options set option_value='http://localhost/' where option_name='siteurl';"
     docker-compose exec -T -u root db mysql -uwordpress -pwordpress wordpress --execute="update wp_options set option_value='http://localhost/' where option_name='home';"
 fi
