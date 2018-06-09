11de199d88820c7ed668833e363a15343dee37c5
Daniel Horton-Wellings <danielhortonwellings@gmail.com>
2018-06-09 11:24:45 -0500

    Removed cap staging config

--- deploy/config/deploy/staging.rb
+++
@@ -1,4 +0,0 @@
-set :stage, :staging
-set :branch, "staging"
-server "192.240.178.212", user: "skinspea", roles: %{web app db}, primary: true
-set :deploy_to, "/home/skinspea/staging.skinspeaks.com/theme"
