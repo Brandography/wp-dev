5382b7310f308c541930641cc04c75c2f09913aa
Daniel Horton-Wellings <danielhortonwellings@gmail.com>
2018-07-11 10:45:54 -0500

    Update db scripts with project name

--- docker/tasks/db_dump.sh
+++ docker/tasks/db_dump.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 cd ../
-docker-compose exec -T -u root db mysqldump -uwordpress -pwordpress wordpress | gzip -c > mysqldump/$(date +%Y-%m-%d-%H.%M.%S).sql.gz
+source ../../.env
+
+docker-compose -p $PROJECT_NAME exec -T -u root db mysqldump -uwordpress -pwordpress wordpress | gzip -c > mysqldump/$(date +%Y-%m-%d-%H.%M.%S).sql.gz
--- docker/tasks/db_reset.sh
+++ docker/tasks/db_reset.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 cd ../
+source ../../.env
+
 if [ -z "$1" ]
   then
     echo "enter sql file name"
 else
-    zcat $@ | docker-compose exec -T -u root db mysql -uwordpress -pwordpress wordpress
-    docker-compose exec -T -u root db mysql -uwordpress -pwordpress wordpress --execute="update wp_options set option_value='http://localhost/' where option_name='siteurl';"
-    docker-compose exec -T -u root db mysql -uwordpress -pwordpress wordpress --execute="update wp_options set option_value='http://localhost/' where option_name='home';"
+    zcat $@ | docker-compose -p $PROJECT_NAME exec -T -u root db mysql -uwordpress -pwordpress wordpress
+    docker-compose -p $PROJECT_NAME exec -T -u root db mysql -uwordpress -pwordpress wordpress --execute="update wp_options set option_value='http://localhost/' where option_name='siteurl';"
+    docker-compose -p $PROJECT_NAME exec -T -u root db mysql -uwordpress -pwordpress wordpress --execute="update wp_options set option_value='http://localhost/' where option_name='home';"
 fi
