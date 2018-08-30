2de4b5fc0eaf8de132e70eac43668bb9290246c5
Daniel Horton-Wellings <danielhortonwellings@gmail.com>
2018-08-30 11:29:03 -0500

    Fixed wp plugin link script

--- tasks/link_plugins.sh
+++ tasks/link_plugins.sh
@@ -5,5 +5,5 @@ for dir in "${dirs[@]}"
 do
     plugin=$(basename "$dir")
     echo $plugin
-    ln -s ../../../src/plugins/$plugin wordpress/wp-content/themes/$plugin
+    ln -s ../../../src/plugins/$plugin wordpress/wp-content/plugins/$plugin
 done
